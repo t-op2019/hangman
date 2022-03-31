d3a717c455ea37701fe458f703fbe7ad91f6d716
t-op2019 <votindu26062003@gmail.com>
2022-03-31 09:05:47 +0700

    Initial Commit

---
+++ hangman/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  hangman
+//
+//  Created by Dư Võ on 3/31/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
